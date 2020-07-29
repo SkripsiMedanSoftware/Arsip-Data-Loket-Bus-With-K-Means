ff491c531e7320d148e04f181662145b547a8994
Agung Dirgantara <agungmasda29@gmail.com>
2020-07-29 20:53:47 +0700

    Update routing for login

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'pengguna';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['site/sign_in'] = 'pengguna/masuk';
